fb6efb0305f903e735f5a1692bbc591ce381c1cf
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-10-14 11:33:07 +0200

    Resetting DLL Queue

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,77 +1,19 @@
-
-
-class Node:
-    def __init__(self, value=None, next=None, prev=None):
-        self.value = value
-        self.next = next
+import sys
+sys.path.append('../doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList
 
 
 class Queue:
     def __init__(self):
         self.size = 0
-        # storage should be initialized with a single Node that contains neither value nor next
-        self.storage = Node()
+        # Why is our DLL a good choice to store our elements?
+        self.storage = DoublyLinkedList()
 
-    # add an item to the back of the queue
     def enqueue(self, value):
-        # Create a new node
-        new_node = Node(value)
+        pass
 
-        # If there's no 'value' for the first item in the queue, we're dealing with an empty cueue
-        if self.storage.value is None:
-            self.storage = new_node
-
-        else:
-            # Set new node's poiner to the current head node
-            new_node.next = self.storage
-            # make new node be at the end of the queue
-            self.storage = new_node
-            
-        # Add one to size
-        self.size += 1
-
-    # remove and return an item from the front of the queue.
     def dequeue(self):
-        # If there's zero items in que, return None
-        if self.size is 0:
-            return None
+        pass
 
-        else:
-            # We need to loop over the queue to reach the first element (where next == none)
-            temp_node = self.storage
-
-            while temp_node.next.value == None:
-                temp_node = temp_node.next
-
-            # Capture value to return it later
-            return_value = temp_node.value
-
-            # Then, we need to set value to None and storage to none
-            temp_node.value = None
-            temp_node.next = None
-
-
-            # Decrement one from self.size
-            self.size -= 1
-            # Return dequeued value
-            print(return_value)
-
-
-    # Returns num of items in queue
     def len(self):
         return self.size
-
-q = Queue()
-q.enqueue(7)
-# print(q.size)
-q.enqueue(10)
-# print(q.size)
-q.enqueue(14)
-# print(q.size)
-
-
-q.dequeue()
-q.dequeue()
-q.dequeue()
-
-
