eddc0a7f3d7f010ce35388b645a85cd34f13245b
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-10-14 10:36:48 +0200

    Queue insert complete

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,19 +1,50 @@
-import sys
-sys.path.append('../doubly_linked_list')
-from doubly_linked_list import DoublyLinkedList
+
+
+class Node:
+    def __init__(self, value=None, next=None):
+        self.value = value
+        self.next = next
 
 
 class Queue:
     def __init__(self):
         self.size = 0
-        # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # storage should be initialized with a single Node that contains neither value nor next
+        self.storage = Node()
 
+    # add an item to the back of the queue
     def enqueue(self, value):
-        pass
+        # Create a new node
+        new_node = Node(value)
+
+        # If there's no 'value' for the first item in the queue, we're dealing with an empty cueue
+        if self.storage.value is None:
+            self.storage = new_node
+
+        else:
+            # connect new Node to previous HEAD node
+            new_node.next = self.storage
+            print(new_node.next)
+            # make new Node new HEAD node
+            self.storage = new_node
+            
+        # Add one to size
+        self.size += 1
 
     def dequeue(self):
-        pass
+        # remove and return an item from the front of the queue.
+        print(self.storage.value)
 
+    # Returns num of items in queue
     def len(self):
-        pass
+        return self.size
+
+q = Queue()
+q.enqueue(7)
+q.enqueue(10)
+q.enqueue(14)
+q.dequeue()
+
+
+
+
